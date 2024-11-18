73558da58172cb0c680801fd971a7ae6cfc7253b
yambenari24 <yam.benari@kaltura.com>
2024-11-18 10:16:53 +0200

    add firebase support ios

--- ios/ToDoList/AppDelegate.mm
+++ ios/ToDoList/AppDelegate.mm
@@ -1,18 +1,25 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <Firebase.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  // Initialize Firebase
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
   self.moduleName = @"ToDoList";
   // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
+
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
